b12094ae836e791a49de131847085f9e5eb0074c
devyu256 <devyu256@gmail.com>
2023-05-01 12:14:29 +0900

    Initial Commit

---
+++ liver/liverApp.swift
@@ -0,0 +1,17 @@
+//
+//  liverApp.swift
+//  liver
+//
+//  Created by 島田雄介 on 2023/05/01.
+//
+
+import SwiftUI
+
+@main
+struct liverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
